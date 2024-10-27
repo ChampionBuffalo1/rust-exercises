3894c4312d4279a2d3d285b91b7b17fdd050c829
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-10-27 22:37:52 +0530

    Completed some chapters

--- exercises/02_basic_calculator/02_variables/src/lib.rs
+++ exercises/02_basic_calculator/02_variables/src/lib.rs
@@ -8,7 +8,8 @@
 pub fn speed(start: u32, end: u32, time_elapsed: u32) -> u32 {
     // TODO: define a variable named `distance` with the right value to get tests to pass
     //  Do you need to annotate the type of `distance`? Why or why not?
-
+    //  We don't need to annotate the type because the compiler will be able to infer the type of 2 u32 on addition operation
+    let distance = end - start;
     // Don't change the line below
     distance / time_elapsed
 }
